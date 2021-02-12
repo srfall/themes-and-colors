28843b24b1875b281a7ef584c1f23dd52deaf06e
srfall <65958585+srfall@users.noreply.github.com>
2021-02-12 11:42:07 +0100

    added pics

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 ## A repository for the ggplot themes and colors i like to use
+
+### Theme one
+<img src="img/one.png" />
+
+
+### Theme two
+<img src="img/two.png" />
+
+
+### Theme three
+<img src="img/three.png" />
---
+++ img/img.R
@@ -0,0 +1,28 @@
+library(here)
+library(ggplot2)
+library(ggtext)
+library(themesNcolors)
+
+
+ test<- data.frame(x=1:100,y=rnorm(100),type=rnorm(100,50,20))
+
+ggplot(test,aes(x=x,y=y,colour=type))+
+  geom_point()+
+  theme_one()
+ggsave(here("img","one.png"),
+            width = 10.5, height = 6.34,
+            device = "png")
+
+ggplot(test,aes(x=x,y=y,colour=type))+
+  geom_point()+
+  theme_two()
+ggsave(here("img","two.png"),
+            width = 10.5, height = 6.34,
+            device = "png")
+
+ggplot(test,aes(x=x,y=y,colour=type))+
+  geom_point()+
+  theme_three()
+ggsave(here("img","three.png"),
+            width = 10.5, height = 6.34,
+            device = "png")
